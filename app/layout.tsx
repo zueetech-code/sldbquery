70c1f032eef5cc7c278392d359f86a727efb1d66
pradeep s <pirathims@gmail.com>
2026-02-21 09:52:26 +0530

    fix name

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,7 +8,7 @@ const _geist = Geist({ subsets: ["latin"] })
 const _geistMono = Geist_Mono({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
-  title: "Zuetech - UCCS",
+  title: "Zuetech - SLDB",
   description: "Created with Zuetech",
   generator: "Zuetech.com",
   icons: {
